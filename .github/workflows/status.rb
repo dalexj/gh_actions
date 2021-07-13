7b0e3628f0d6f91618778a5663df4a87c9012cfa
Alex Jensen <alex.jensen@hotelengine.com>
2021-07-12 21:48:21 -0600

    some stufffs

--- .github/workflows/status.rb
+++ .github/workflows/status.rb
@@ -1,4 +1,22 @@
 puts "hello from inside #{ENV["ALEX_VAR"]} #{ENV["ALEX_VAR"].to_s.length}"
-puts `git remote show origin`
-puts `git checkout -b new-branch`
-puts `git push -u origin new-branch`
+
+GIT_LOG_FORMAT = "format:• <https://github.com/HotelEngine/he-api/commit/%H|%s> - %an".freeze
+
+latest_tag = `git describe --abbrev=0 --tags --always`.chomp
+formatted_recent_commits = `git log #{latest_tag}..HEAD --pretty="#{GIT_LOG_FORMAT}"`
+
+def increment_minor_version(tag)
+  split_versions = tag.split(".")
+  [split_versions[0], (split_versions[1].to_i + 1), 0].join(".")
+end
+
+version = increment_minor_version(latest_tag)
+
+
+tag_file = Tempfile.new
+tag_file.write("Version #{version}\n\n#{formatted_recent_commits}\n\n")
+puts "git tag -a #{version} --cleanup=verbatim --file #{tag_file.path}"
+puts `git tag -a #{version} --cleanup=verbatim --file #{tag_file.path}`
+
+puts "git push origin #{version}"
+puts `git push origin #{version}`
